4790b312e8a3e249104aa5773a05b3955318c270
Ritesh Sinha <Ritesh.Sinha@mLogica.com>
2021-09-17 15:48:59 +0100

    Add project files.

---
+++ AudenTest1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AudenTest1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("AudenTest1")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8e39773b-fc32-4d4c-a26c-60745cfc38c8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AudenTest1/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+
+namespace AudenTest1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("Start Maximed");
+            IWebDriver webDriver = new ChromeDriver(options);
+            WebDriverWait wait = new WebDriverWait(webDriver, TimeSpan.FromSeconds(30));
+            webDriver.Navigate().GoToUrl("https://www.auden.com/short-term-loan");
+            IWebElement textField = webDriver.FindElement(By.Name("loan-amount-value"));
+            IWebElement sliderbutton = webDriver.FindElement(By.XPath('//"[@id="root"]/main/div/section/section[1]/div"));
+           
+
+
+        }
+    }
+}
